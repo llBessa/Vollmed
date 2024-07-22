6b17b22f0f3823cb159fe7f0eab5280a569d7c8f
Lucas Bessa <lucas.bessafp72@gmail.com>
2024-07-22 16:28:14 -0400

    feat: agendamento e cancelamento de consultas

---
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,39 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Consulta")
+@Table(name = "consultas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+
+    @Enumerated(EnumType.STRING)
+    private MotivoCancelamento motivoCancelamento;
+
+}
---
+++ src/main/java/med/voll/api/service/ConsultaService.java
@@ -0,0 +1,60 @@
+package med.voll.api.service;
+
+import lombok.RequiredArgsConstructor;
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.infra.exception.ValidacaoException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConsultaService {
+
+    private final ConsultaRepository repository;
+    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
+
+    public Consulta agendar(DadosAgendamentoConsulta dadosAgendamento) throws ValidacaoException{
+        if(!pacienteRepository.existsById(dadosAgendamento.idPaciente())){
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        if(dadosAgendamento.idMedico() != null && !medicoRepository.existsById(dadosAgendamento.idMedico())){
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
+        Consulta consulta = new Consulta();
+        consulta.setMedico(escolheMedicoAleatorio(dadosAgendamento));
+        consulta.setPaciente(pacienteRepository.getReferenceById(dadosAgendamento.idPaciente()));
+        consulta.setData(dadosAgendamento.data());
+        repository.save(consulta);
+        return consulta;
+    }
+
+    private Medico escolheMedicoAleatorio(DadosAgendamentoConsulta dadosAgendamento) throws ValidacaoException {
+        if (dadosAgendamento.idMedico() != null){
+            return medicoRepository.getReferenceById(dadosAgendamento.idMedico());
+        }
+
+        if (dadosAgendamento.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dadosAgendamento.especialidade(), dadosAgendamento.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) throws ValidacaoException {
+        if (!repository.existsById(dados.idConsulta())){
+            throw new ValidacaoException("Id da consulta não existe");
+        }
+
+        Consulta consulta = repository.getReferenceById(dados.idConsulta());
+        consulta.setMotivoCancelamento(dados.motivo());
+    }
+}
